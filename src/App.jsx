4a11cbeb838688a431c1d0563c6306fdc568d234
hackcharms <zaa78692@gmail.com>
2024-06-21 11:29:56 +0530

    basic structor, data binding and jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from "react";
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const msg = "Hello World! I'm a variable";
+  const myStyle = {
+    color: "green",
+    backgroundColor: "yellow",
+  };
+  // const showDat
   return (
+    // <React.Fragment>
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1 className="card">{msg}</h1>
+      <p
+        style={{
+          color: "green",
+          backgroundColor: "yellow",
+        }}
+      >
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere,
+        laborum.
       </p>
+      {/* <input type="text" value={msg} /> */}
+      <div>
+        {/* <Button></Button>
+        <Button></Button>
+        <Button></Button> */}
+      </div>
     </>
-  )
+  );
 }
+export default App;
 
-export default App
+function Button() {
+  return <button type="button">Btn</button>;
+}
