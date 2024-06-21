7e9207cd3f58d2ef83a816d9fe4d57ceca956275
hackcharms <zaa78692@gmail.com>
2024-06-21 11:52:59 +0530

    conditional rendering showcase

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import "./App.css";
 function App() {
   const msg = "Hello World! I'm a variable";
-  const myStyle = {
-    color: "green",
-    backgroundColor: "yellow",
-  };
-  // const showDat
+  const showDiv1 = true;
+  // let content = "";
+  const div1 = `Hello div1`;
+  // const div2 = `Hello div2`;
+  // content = showDiv1 ? div1 : div2;
+  // if (showDiv1) {
+  //   content = div1;
+  // } else {
+  //   content = div2;
+  // }
   return (
     // <React.Fragment>
     <>
@@ -20,6 +25,8 @@ function App() {
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere,
         laborum.
       </p>
+      <p>content using ternary = {showDiv1 ? div1 : ""} </p>
+      <p>content using short circuit= {showDiv1 && div1} </p>
       {/* <input type="text" value={msg} /> */}
       <div>
         {/* <Button></Button>
