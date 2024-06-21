f855affd083fa480604acceb13355084d9864280
hackcharms <zaa78692@gmail.com>
2024-06-21 12:01:58 +0530

    list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,19 @@ import React from "react";
 import "./App.css";
 function App() {
   const msg = "Hello World! I'm a variable";
-  const showDiv1 = true;
-  // let content = "";
-  const div1 = `Hello div1`;
-  // const div2 = `Hello div2`;
-  // content = showDiv1 ? div1 : div2;
-  // if (showDiv1) {
-  //   content = div1;
-  // } else {
-  //   content = div2;
-  // }
+  const products = [
+    { title: "Cabbage", isFruit: false, id: 1 },
+    { title: "Garlic", isFruit: false, id: 2 },
+    { title: "Apple", isFruit: true, id: 3 },
+  ];
   return (
     // <React.Fragment>
     <>
       <h1 className="card">{msg}</h1>
-      <p
-        style={{
-          color: "green",
-          backgroundColor: "yellow",
-        }}
-      >
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere,
-        laborum.
-      </p>
-      <p>content using ternary = {showDiv1 ? div1 : ""} </p>
-      <p>content using short circuit= {showDiv1 && div1} </p>
-      {/* <input type="text" value={msg} /> */}
       <div>
-        {/* <Button></Button>
-        <Button></Button>
-        <Button></Button> */}
+        {products.map((product) => {
+          return <h1 key={product.id}>{product.title}</h1>;
+        })}
       </div>
     </>
   );
